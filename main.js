9e5c119c4158192facf2c97d85f17ff773bb0068
pasqualedimaro <pakydm15@gmail.com>
2025-06-05 16:29:55 +0200

    add switch on/off

--- main.js
+++ main.js
@@ -3,7 +3,22 @@
 const lampadina = document.getElementById('lampadina');
 const bottone = document.getElementById('bottone');
 
-bottone.addEventListener('click', function() {
+/*bottone.addEventListener('click', function() {
     console.log('Cliccato!');
     lampadina.src = 'img/yellow_lamp.png';
+});*/
+
+//devo fare anche lo spegnimento
+
+bottone.addEventListener('click', function() {
+    console.log('Cliccato!');
+    // provo ad usare if
+
+   if (bottone.innerHTML === 'Accendi') {
+        lampadina.src = 'img/yellow_lamp.png';
+        bottone.innerHTML = 'Spegni';
+    } else {
+        lampadina.src = 'img/white_lamp.png';
+        bottone.innerHTML = 'Accendi';
+    }
 });
