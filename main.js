e55c77c80891fb82846f8f6b7017bf95e057e525
pasqualedimaro <pakydm15@gmail.com>
2025-06-05 16:37:02 +0200

    extract toggle function

--- main.js
+++ main.js
@@ -1,21 +1,14 @@
-// creo il click
-// salvo elementi in variabili
 const lampadina = document.getElementById('lampadina');
 const bottone = document.getElementById('bottone');
 
-/*bottone.addEventListener('click', function() {
-    console.log('Cliccato!');
-    lampadina.src = 'img/yellow_lamp.png';
-});*/
-
-//devo fare anche lo spegnimento
-
-bottone.addEventListener('click', function() {
-     if (bottone.innerHTML === 'Accendi') {
+function toggleLampadina() {
+    if (bottone.innerHTML === 'Accendi') {
         lampadina.src = 'img/yellow_lamp.png';
         bottone.innerHTML = 'Spegni';
     } else {
         lampadina.src = 'img/white_lamp.png';
         bottone.innerHTML = 'Accendi';
     }
-});
+}
+
+bottone.addEventListener('click', toggleLampadina);
