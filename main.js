b8615f678d046141fd995c8bf75669cad814fce2
pasqualedimaro <pakydm15@gmail.com>
2025-06-05 16:44:06 +0200

    improve variamble names and adjusted comments

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const lampadina = document.getElementById('lampadina');
-const bottone = document.getElementById('bottone');
+// Seleziono gli elementi dal dom
+const imgLampadina = document.getElementById('lampadina');
+const btnToggle = document.getElementById('bottone');
 
+// Funzione per accendere/spegnere la lampadina
 function toggleLampadina() {
-    if (bottone.innerHTML === 'Accendi') {
-        lampadina.src = 'img/yellow_lamp.png';
-        bottone.innerHTML = 'Spegni';
+    if (btnToggle.innerHTML === 'Accendi') {
+        // Accendi la lampadina
+        imgLampadina.src = 'img/yellow_lamp.png';
+        btnToggle.innerHTML = 'Spegni';
     } else {
-        lampadina.src = 'img/white_lamp.png';
-        bottone.innerHTML = 'Accendi';
+        // Spegni la lampadina
+        imgLampadina.src = 'img/white_lamp.png';
+        btnToggle.innerHTML = 'Accendi';
     }
 }
 
-bottone.addEventListener('click', toggleLampadina);
+// Aggiungo l'evento click al bottone
+btnToggle.addEventListener('click', toggleLampadina);
