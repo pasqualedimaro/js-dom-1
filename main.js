1400be331d99b3a535a6c17522aec84becab95f1
pasqualedimaro <pakydm15@gmail.com>
2025-06-05 15:54:06 +0200

    button click work, change with img

--- main.js
+++ main.js
@@ -2,4 +2,5 @@
 
 document.getElementById('bottone').addEventListener('click', function() {
     console.log('Cliccato!');
+    document.getElementById('lampadina').src = 'img/yellow_lamp.png';
 });
